578439505ff57a7153581a725c51b69c6164d4b0
Matteo Federico Zazzetta <fulminedipegasus@gmail.con>
2020-03-08 15:39:54 +0100

    Fixed build script

--- p4a.sh
+++ p4a.sh
@@ -1,4 +1,5 @@
 # p4a clean_builds && p4a clean_dists
+cd /home/matteo/python-for-android/pymoviz
 cp main/main.main.py src/main.py
 p4a apk --private /home/matteo/python-for-android/pymoviz/src --package=org.kivymfz.pymoviz --name "PyMoviz" --version 1.0 --bootstrap=sdl2 --requirements=libffi,python3,python-osc,kivy,setuptools,kivymd,aiosqlite,able --debug --permission INTERNET --permission WRITE_EXTERNAL_STORAGE --permission READ_EXTERNAL_STORAGE --permission FOREGROUND_SERVICE --permission ACCESS_COARSE_LOCATION --dist-name pymoviz_apk --service=DeviceManagerService:./service/device_manager_service.py
 
--- p4a.tst.sh
+++ p4a.tst.sh
@@ -1,4 +1,5 @@
 # p4a clean_builds && p4a clean_dists
+cd /home/matteo/python-for-android/pymoviz
 cp main/main.tst.py src/main.py
 p4a apk --private /home/matteo/python-for-android/pymoviz/src --package=org.kivymfz.pymoviz.test --name "PyMoviz" --version 1.0 --bootstrap=sdl2 --requirements=libffi,python3,python-osc,kivy,setuptools,kivymd,aiosqlite,able --debug --permission INTERNET --permission WRITE_EXTERNAL_STORAGE --permission READ_EXTERNAL_STORAGE --permission FOREGROUND_SERVICE --permission ACCESS_COARSE_LOCATION --dist-name pymoviz_test_apk --service=BluetoothService:./service/bluetooth_service.py
 
