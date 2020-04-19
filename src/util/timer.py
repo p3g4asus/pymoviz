7cf8b77f05bbffd88439d700c5f053e788b52b81
Matteo Federico Zazzetta <fulminedipegasus@gmail.con>
2020-04-19 14:19:23 +0200

    Fixes

--- src/util/timer.py
+++ src/util/timer.py
@@ -6,6 +6,7 @@ class Timer:
     def __init__(self, timeout, callback, loop=None):
         self._timeout = timeout
         self._callback = callback
+        self.loop = loop
         self._task = asyncio.ensure_future(self._job(), loop=loop)
         fname = callback.__name__ if not isinstance(callback, partial) else callback.func.__name__
         self._task.name = f'_timer_{fname}'
@@ -14,16 +15,25 @@ class Timer:
     def task_is_timer(task):
         return hasattr(task, 'name') and task.name.startswith('_timer_')
 
+    async def _job_cancel(self):
+        self._task.cancel()
+        try:
+            await self._task
+        except asyncio.CancelledError:
+            pass
+
     async def _job(self):
         try:
             if self._timeout:
                 await asyncio.sleep(self._timeout)
             await self._callback()
             # self._task.set_result(0)
-        except (asyncio.CancelledError, Exception):
+        except asyncio.CancelledError:
+            raise
+        except Exception:
             # self._task.set_result(ex)
             pass
 
     def cancel(self):
         if not self._task.done():
-            self._task.cancel()
+            asyncio.ensure_future(self._job_cancel(), loop=self.loop)
