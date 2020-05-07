0ef3c1546fbfc94dd0ce09412cfa4ba563eeddd1
Matteo Federico Zazzetta <fulminedipegasus@gmail.con>
2020-05-07 16:04:24 +0200

    Fixes

--- src/util/android_alive_checker.py
+++ src/util/android_alive_checker.py
@@ -12,31 +12,29 @@ class AndroidAliveChecker(object):
     def __init__(self, loop, on_result, timeout=1.2):
         self.timeout = timeout
         self.loop = loop
-        self.started = False
         self.on_result = on_result
         self.timer = None
         self.was_started = False
         if platform == 'android':
             from jnius import autoclass
-            from android.broadcast import BroadcastReceiver
             self.context = autoclass('org.kivy.android.PythonActivity').mActivity
             self.Intent = autoclass('android.content.Intent')
-            self.br = BroadcastReceiver(
-                self.on_broadcast, actions=[PRESENCE_RESPONSE_ACTION])
-        else:
-            self.br = None
+        self.br = None
 
     def on_broadcast(self, context, intent):
         _LOGGER.info('Broadcast received')
         self.loop.call_soon_threadsafe(self.on_timeout, False)
 
     def on_pause(self):
-        if self.started:
-            self.was_started = self.started
+        if self.br:
+            self.was_started = True
             self.stop()
+        else:
+            self.was_started = False
 
     def on_resume(self):
         if self.was_started:
+            self.was_started = False
             self.start()
 
     def on_timeout(self, timeout_detected=True):
@@ -45,17 +43,19 @@ class AndroidAliveChecker(object):
         self.on_result(not timeout_detected)
 
     def start(self, bytimer=False):
-        _LOGGER.info(f'Starting {bytimer} timerNone={self.timer is None} started={self.started} brNone={self.br is None}')
+        _LOGGER.info(f'Starting {bytimer} timerNone={self.timer is None} brNone={self.br is None}')
         try:
             if not self.timer:
                 if bytimer:
                     self.timer = Timer(bytimer, self.start)
-                elif not self.started:
-                    if self.br:
+                elif not self.br:
+                    if platform == 'android':
+                        from android.broadcast import BroadcastReceiver
+                        self.br = BroadcastReceiver(
+                            self.on_broadcast, actions=[PRESENCE_RESPONSE_ACTION])
                         self.br.start()
                         _LOGGER.info(f'Sending intent {PRESENCE_REQUEST_ACTION}')
                         self.context.sendBroadcast(self.Intent(PRESENCE_REQUEST_ACTION))
-                        self.started = True
                         self.timer = Timer(self.timeout, self.on_timeout)
                     else:
                         self.on_result(False)
@@ -66,6 +66,6 @@ class AndroidAliveChecker(object):
         if self.timer:
             self.timer.cancel()
             self.timer = None
-        if self.started:
+        if self.br:
             self.br.stop()
-            self.started = False
+            self.br = None
