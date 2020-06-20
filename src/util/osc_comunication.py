7a29a7d99f2ce7b7a716b2c8eb1be282b5148544
Matteo Federico Zazzetta <fulminedipegasus@gmail.con>
2020-06-21 01:19:57 +0200

    Version 2 tst

--- src/util/osc_comunication.py
+++ src/util/osc_comunication.py
@@ -268,6 +268,8 @@ class OSCManager(object):
                                            **p['kwargs'],
                                            last_sent=time())
                 for hpstr, d in self.connected_hosts.items():
+                    if el['address'] != COMMAND_CONNECTION:
+                        _LOGGER.debug(f'Maybe Sending {el["dest"]} = {hpstr}')
                     if not el['dest'] or hpstr == f'{el["dest"][0]}:{el["dest"][1]}':
                         if el['address'] != COMMAND_CONNECTION:
                             _LOGGER.debug(f'Sending[{d["hp"][0]}:{d["hp"][1]}] {el["address"]} -> {args}')
