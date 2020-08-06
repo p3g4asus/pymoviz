5260256b24668894713b65fbde6833d76a5a8f72
Matteo Federico Zazzetta <fulminedipegasus@gmail.con>
2020-08-06 17:11:45 +0200

    Req Fix

--- p4a.sh
+++ p4a.sh
@@ -2,6 +2,6 @@
 # find /home/matteo/.local/share/python-for-android -name kivymd* -print0 | xargs -0 rm -Rf
 cd /home/matteo/python-for-android/pymoviz
 cp main/main.main.py src/main.py
-p4a apk --private /home/matteo/python-for-android/pymoviz/src --package=org.kivymfz.pymoviz --name "PyMoviz" --version 1.0 --bootstrap=sdl2 --requirements=libffi,python3,python-osc,kivy,setuptools,kivymd,aiosqlite,able,airspeed,pyjnius --debug --permission INTERNET --permission WRITE_EXTERNAL_STORAGE --permission READ_EXTERNAL_STORAGE --permission FOREGROUND_SERVICE --permission ACCESS_FINE_LOCATION --permission BLUETOOTH --permission BLUETOOTH_ADMIN --dist-name pymoviz_apk --service=DeviceManagerService:./service/device_manager_service.py
+p4a apk --private /home/matteo/python-for-android/pymoviz/src --package=org.kivymfz.pymoviz --name "PyMoviz" --version 1.0 --bootstrap=sdl2 --requirements=libffi,python3,python-osc,certifi,kivy,setuptools,kivymd,aiosqlite,able,airspeed,pyjnius --debug --permission INTERNET --permission WRITE_EXTERNAL_STORAGE --permission READ_EXTERNAL_STORAGE --permission FOREGROUND_SERVICE --permission ACCESS_FINE_LOCATION --permission BLUETOOTH --permission BLUETOOTH_ADMIN --dist-name pymoviz_apk --service=DeviceManagerService:./service/device_manager_service.py
 
 # # cd /home/matteo/.local/share/python-for-android/dists/pymoviz_apk && /home/matteo/.loal/share/python-for-android/dists/pymoviz_apk/gradlew assembleDebug
